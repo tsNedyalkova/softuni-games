376e28a468e187e01575279eae9392b0090cf166
tsNedyalkova <ts.nedyalkova@abv.bg>
2024-08-16 15:06:28 +0300

    Add navigation links

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
-    {/* <!-- Navigation --> */}
-    <h1><a class="home" href="#">GamesPlay</a></h1>
-    <nav>
-        <a href="#">All games</a>
+      {/* <!-- Navigation --> */}
+      <h1>
+        <Link class="home" to="/">
+          GamesPlay
+        </Link>
+      </h1>
+      <nav>
+        <Link to="/games">All games</Link>
         {/* <!-- Logged-in users --> */}
         <div id="user">
-            <a href="#">Create Game</a>
-            <a href="#">Logout</a>
+          <Link to="/games/create">Create Game</Link>
+          <Link to="/logout">Logout</Link>
         </div>
         {/* <!-- Guest users --> */}
         <div id="guest">
-            <a href="#">Login</a>
-            <a href="#">Register</a>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
         </div>
-    </nav>
-</header>
-  )
+      </nav>
+    </header>
+  );
 }
