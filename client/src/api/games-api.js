60895aba9ec3c52ccad8ec4f5a59018b681630dd
tsNedyalkova <ts.nedyalkova@abv.bg>
2024-08-20 17:04:32 +0300

    Implement create-game functionality

--- client/src/api/games-api.js
+++ client/src/api/games-api.js
@@ -1,6 +1,6 @@
 import * as requester from './requester.js';
 
-const BASE_URL = 'http://localhost:3030/jsonstore/games';
+const BASE_URL = 'http://localhost:3030/data/games';
 
 export const getAll = async () => {
     const result = await requester.get(BASE_URL);
@@ -12,9 +12,13 @@ export const getAll = async () => {
 
 export const getOne = (gameId) => requester.get(`${BASE_URL}/${gameId}`);
 
+export const create = (gameData) => requester.post(`${BASE_URL}`, gameData);
+
 const gamesAPI = {
     getAll,               
     getOne,
+    create
+
 }
 
 export default gamesAPI;
--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/use-form";
+import { useCreateGame } from "../../hooks/use-games";
+
+const initialValues = {
+  title: "",
+  category: "",
+  maxLevel: "",
+  imageUrl: "",
+  summary: "",
+};
 
 export default function GameCreate() {
+  const navigate = useNavigate();
+  const createGame = useCreateGame();
+
+  const createHandler = async (values) => {
+    try {
+      const { _id: gameId } = await createGame(values);
+      // console.log(values);
+      navigate(`/games/${gameId}/details`);
+    } catch (error) {
+      // TODO: Set error state and display error
+      console.log(error.message);
+    }
+  };
+
+  const { values, changeHandler, submitHandler } = useForm(
+    initialValues,
+    createHandler
+  );
+
   return (
     <section id="create-page" className="auth">
-      <form id="create">
+      <form id="create" onSubmit={submitHandler}>
         <div className="container">
           <h1>Create Game</h1>
           <label htmlFor="leg-title">Legendary title:</label>
@@ -11,6 +40,8 @@ export default function GameCreate() {
             type="text"
             id="title"
             name="title"
+            value={values.title}
+            onChange={changeHandler}
             placeholder="Enter game title..."
           />
 
@@ -19,6 +50,8 @@ export default function GameCreate() {
             type="text"
             id="category"
             name="category"
+            value={values.category}
+            onChange={changeHandler}
             placeholder="Enter game category..."
           />
 
@@ -27,6 +60,8 @@ export default function GameCreate() {
             type="number"
             id="maxLevel"
             name="maxLevel"
+            value={values.maxLevel}
+            onChange={changeHandler}
             min="1"
             placeholder="1"
           />
@@ -36,11 +71,18 @@ export default function GameCreate() {
             type="text"
             id="imageUrl"
             name="imageUrl"
+            value={values.imageUrl}
+            onChange={changeHandler}
             placeholder="Upload a photo..."
           />
 
           <label htmlFor="summary">Summary:</label>
-          <textarea name="summary" id="summary"></textarea>
+          <textarea
+            name="summary"
+            value={values.summary}
+            onChange={changeHandler}
+            id="summary"
+          ></textarea>
           <input className="btn submit" type="submit" value="Create Game" />
         </div>
       </form>
